a2573679ef69cc32a607631bca9101f8f18b6aaa
Lauren Champs <laurenkchamps@gmail.com>
2023-09-01 12:23:06 +0930

    Add basic styling and content

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Hello from JS file')
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.isRead = isRead
+}
+
+function addBookToLibrary(){
+    
+}
+
+const earthsea = new Book(
+    'Earthsea',
+    'Ursula K. Le Guin',
+    704,
+    false,
+)
+
+const nineteenEightyFour = new Book(
+    'Nineteen Eighty-Four',
+    'George Orwell',
+    400,
+    true,
+)
+
+myLibrary.push(earthsea, nineteenEightyFour);
+
+console.log(myLibrary);
