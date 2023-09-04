908792712ab5d7ff31343b2241835b84455e741c
Lauren Champs <laurenkchamps@gmail.com>
2023-09-04 15:59:05 +0930

    Make 'remove' button functional for each book

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ const isRead = document.getElementById('read-checkbox');
 
 const cardGroup = document.querySelector('.card-group');
 
+const toggle = document.querySelectorAll('.toggle');
+const removeBtns = document.querySelectorAll('.remove');
+
+
 function Book(title, author, pages, isRead) {
     this.title = title
     this.author = author
@@ -33,10 +37,10 @@ function displayLibrary() {
     while (cardGroup.firstChild) {
         cardGroup.removeChild(cardGroup.firstChild);
     }
-
+    
     // Add card for each book in library
     myLibrary.forEach(function(item, index) {
-
+        
         // Create card and child elements
         const card = document.createElement('div');
         const title = document.createElement('p');
@@ -47,7 +51,7 @@ function displayLibrary() {
         const toggleInput = document.createElement('input');
         const removeBtn = document.createElement('button');
         const btnGrp = document.createElement('div');
-
+        
         // Set attributes on elements
         card.className = 'card';
         card.setAttribute('id', index);
@@ -55,83 +59,95 @@ function displayLibrary() {
         author.className = 'author';
         pages.className = 'pages';
         toggleDiv.className = 'toggle-checkbox';
-        toggleLabel.setAttribute('for', 'toggle');
+        toggleLabel.setAttribute('for', `toggle-${index}`);
         toggleInput.setAttribute('type', 'checkbox');
         toggleInput.setAttribute('name', 'isRead');
-        toggleInput.setAttribute('id', 'toggle');
+        toggleInput.setAttribute('id', `toggle-${index}`);
         toggleInput.className = 'toggle';
         removeBtn.className = 'remove';
         btnGrp.className = 'wrapper';
-
+        
         if (item.isRead === true) {
             toggleInput.checked = true;
+            toggleInput.className += ' read';
         } 
 
+        removeBtn.addEventListener('click', removeBookFromLibrary);
+        
         // Add text 
         title.appendChild(document.createTextNode(item.title));
         author.appendChild(document.createTextNode(item.author));
         pages.appendChild(document.createTextNode(item.pages + ' pages'));
         toggleLabel.appendChild(document.createTextNode('read?'));
         removeBtn.appendChild(document.createTextNode('Remove'));
-
+        
         // Add elements to DOM
         toggleDiv.appendChild(toggleLabel);
         toggleDiv.appendChild(toggleInput);
-
+        
         btnGrp.appendChild(toggleDiv);
         btnGrp.appendChild(removeBtn);
-
+        
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
         card.appendChild(btnGrp);
-
+        
         cardGroup.appendChild(card);
     })
 }
 
-function addBookToLibrary(e) {
-    e.preventDefault();
+displayLibrary();
 
+function addBookToLibrary(e) {
+    // e.preventDefault();
+    
     const bookTitle = title.value;
     const bookAuthor = author.value;
     const bookPages = Number(pages.value);
     let bookIsRead = isRead.checked;
-
+    
     const newBook = new Book(
         bookTitle,
         bookAuthor,
         bookPages,
         bookIsRead
-    )
-
-    if (myLibrary.some(item => item.title === bookTitle && item.author === bookAuthor)) {
-        alert('Book already exists in your library') 
-    } else {
-        myLibrary.push(newBook);
-    }
+        )
+        
+        if (myLibrary.some(item => item.title === bookTitle && item.author === bookAuthor)) {
+            alert('Book already exists in your library') 
+        } else {
+            myLibrary.push(newBook);
+        }
+        
+        displayLibrary();
+        modal.close();
+        
+        // Reset form values
+        
+        title.value = '';
+        author.value = '';
+        pages.value = '';
+        isRead.checked = false;
+}
+    
+function removeBookFromLibrary(e) {
+    myLibrary.splice(e.target.parentElement.parentElement.id, 1);
 
     displayLibrary();
-
-    // Reset form values
-
-    title.value = '';
-    author.value = '';
-    pages.value = '';
-    isRead.checked = false;
 }
-
-
+    
 // Event listeners
-
+    
 openModal.addEventListener('click', () => {
     modal.showModal();
 })
-
+    
 closeModal.addEventListener('click', () => {
     modal.close();
 })
 
 submit.addEventListener('click', addBookToLibrary);
 
-window.addEventListener('load', displayLibrary);
+
+
