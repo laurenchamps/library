33bb9b999581efd952281ceb656a04c39a54d928
Lauren Champs <laurenkchamps@gmail.com>
2023-09-05 16:40:07 +0930

    Amend 'read' toggle to be accessible

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function Book(title, author, pages, isRead) {
 }
 
 const theHobbit = new Book('The Hobbit', 'JRR Tolkien', 700, true);
-const nineteenEightyFour = new Book('1984', 'George Orwell', 400, true);
+const nineteenEightyFour = new Book('1984', 'George Orwell', 400, false);
 
 myLibrary.push(theHobbit);
 myLibrary.push(nineteenEightyFour);
@@ -47,9 +47,7 @@ function displayLibrary() {
         const title = document.createElement('p');
         const author = document.createElement('p');
         const pages = document.createElement('p');
-        const toggleDiv = document.createElement('div');
-        const toggleLabel = document.createElement('label');
-        const toggleInput = document.createElement('input');
+        const toggleBtn = document.createElement('button');
         const removeBtn = document.createElement('button');
         const btnGrp = document.createElement('div');
         
@@ -59,35 +57,26 @@ function displayLibrary() {
         title.className = 'title';
         author.className = 'author';
         pages.className = 'pages';
-        toggleDiv.className = 'toggle-checkbox';
-        toggleLabel.setAttribute('for', `toggle-${index}`);
-        toggleInput.setAttribute('type', 'checkbox');
-        toggleInput.setAttribute('name', 'isRead');
-        toggleInput.setAttribute('id', `toggle-${index}`);
-        toggleInput.className = 'toggle';
+        toggleBtn.className = 'toggle';
+        toggleBtn.setAttribute('type', 'button');
+
         removeBtn.className = 'remove';
-        btnGrp.className = 'wrapper';
+        btnGrp.className = 'button-group';
         
         if (item.isRead === true) {
-            toggleInput.checked = true;
-            toggleInput.className += ' read';
-        } 
-
-        removeBtn.addEventListener('click', removeBookFromLibrary);
-        toggleInput.addEventListener('click', toggleRead);
+            toggleBtn.setAttribute('aria-pressed', true);
+        } else {
+            toggleBtn.setAttribute('aria-pressed', false);
+        }
         
         // Add text 
         title.appendChild(document.createTextNode(item.title));
         author.appendChild(document.createTextNode(item.author));
         pages.appendChild(document.createTextNode(item.pages + ' pages'));
-        toggleLabel.appendChild(document.createTextNode('read?'));
         removeBtn.appendChild(document.createTextNode('Remove'));
         
-        // Add elements to DOM
-        toggleDiv.appendChild(toggleLabel);
-        toggleDiv.appendChild(toggleInput);
-        
-        btnGrp.appendChild(toggleDiv);
+        // Add elements to DOM    
+        btnGrp.appendChild(toggleBtn);
         btnGrp.appendChild(removeBtn);
         
         card.appendChild(title);
@@ -96,6 +85,10 @@ function displayLibrary() {
         card.appendChild(btnGrp);
         
         cardGroup.appendChild(card);
+
+        // Create event listeners
+        removeBtn.addEventListener('click', removeBookFromLibrary);
+        toggleBtn.addEventListener('click', toggleRead);
     })
 }
 
@@ -140,13 +133,24 @@ function removeBookFromLibrary(e) {
 }
 
 function toggleRead(e) {
-    if(e.target.className.includes('read')) {
-        e.target.classList.remove('read');
-        myLibrary[e.target.parentElement.parentElement.parentElement.getAttribute('data-index')].isRead = false;
-    } else {
-        e.target.classList.add('read');
-        myLibrary[e.target.parentElement.parentElement.parentElement.getAttribute('data-index')].isRead = true;
-    };
+    if (e.target.ariaPressed === 'true') {
+        e.target.ariaPressed = 'false';
+        myLibrary[e.target.parentElement.parentElement.getAttribute('data-index')].isRead = false;
+    } else if (e.target.ariaPressed === 'false') {
+        e.target.ariaPressed = 'true';
+        myLibrary[e.target.parentElement.parentElement.getAttribute('data-index')].isRead = true;
+    }
+    // else {
+    //     e.target.ariaPressed = 'true';
+    //     myLibrary[e.target.parentElement.parentElement.getAttribute('data-index')].isRead = true;
+    // }
+    // if(e.target.className.includes('read')) {
+    //     e.target.classList.remove('read');
+    //     myLibrary[e.target.parentElement.parentElement.parentElement.getAttribute('data-index')].isRead = false;
+    // } else {
+    //     e.target.classList.add('read');
+    //     myLibrary[e.target.parentElement.parentElement.parentElement.getAttribute('data-index')].isRead = true;
+    // };
 }
     
 // Event listeners
