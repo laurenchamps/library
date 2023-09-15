7fd746cec5cd2dd0bf192c701438315b272990b8
Lauren Champs <laurenkchamps@gmail.com>
2023-09-15 16:32:39 +0930

    Refactor into classes

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const myLibrary = [];
-
 const modal = document.querySelector('dialog');
 const openModal = document.querySelector('.open-modal');
 const closeModal = document.querySelector('.close-modal');
@@ -16,13 +14,40 @@ const cardGroup = document.querySelector('.card-group');
 const toggle = document.querySelectorAll('.toggle');
 const removeBtns = document.querySelectorAll('.remove');
 
+class Book {
+    constructor(title, author, pages, isRead) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.isRead = isRead
+    }
 
-function Book(title, author, pages, isRead) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.isRead = isRead
+    toggleRead(e) {
+        if (e.target.ariaPressed === 'true') {
+            e.target.ariaPressed = 'false';
+            myLibrary[e.target.parentElement.parentElement.getAttribute('data-index')].isRead = false;
+        } else if (e.target.ariaPressed === 'false') {
+            e.target.ariaPressed = 'true';
+            myLibrary[e.target.parentElement.parentElement.getAttribute('data-index')].isRead = true;
+        }
+    }
 }
+
+class Library {
+    constructor() {
+        this.books = []
+    }
+
+    removeBook(title) {
+        this.books = this.books.filter(book => book.title !== title);
+    }
+
+    addBook(newBook) {
+        this.books.push(newBook);
+    }
+}
+
+const myLibrary = new Library();
 
 const mountain = new Book(
     'The Living Mountain',
@@ -45,126 +70,117 @@ const trial = new Book(
     false
 )
 
-myLibrary.push(mountain, wolves, trial);
+myLibrary.addBook(mountain);
+myLibrary.addBook(wolves);
+myLibrary.addBook(trial);
 
-function displayLibrary() {
-    // Remove all existing book cards
-    while (cardGroup.firstChild) {
-        cardGroup.removeChild(cardGroup.firstChild);
-    }
-    
-    // Add card for each book in library
-    myLibrary.forEach(function(item, index) {
-        
-        // Create card and child elements
-        const card = document.createElement('div');
-        const title = document.createElement('p');
-        const author = document.createElement('p');
-        const pages = document.createElement('p');
-        const toggleBtn = document.createElement('button');
-        const removeBtn = document.createElement('button');
-        const btnGrp = document.createElement('div');
-        
-        // Set attributes on elements
-        card.className = 'card';
-        card.setAttribute('data-index', index);
-        title.className = 'title';
-        author.className = 'author';
-        pages.className = 'pages';
-        toggleBtn.className = 'toggle';
-        removeBtn.className = 'remove';
-        btnGrp.className = 'button-group';
-        
-        if (item.isRead === true) {
-            toggleBtn.setAttribute('aria-pressed', true);
-        } else {
-            toggleBtn.setAttribute('aria-pressed', false);
-        }
-        
-        // Add text 
-        title.appendChild(document.createTextNode(item.title));
-        author.appendChild(document.createTextNode(item.author));
-        pages.appendChild(document.createTextNode(item.pages + ' pages'));
-        removeBtn.appendChild(document.createTextNode('Remove'));
-        
-        // Add elements to DOM    
-        btnGrp.appendChild(toggleBtn);
-        btnGrp.appendChild(removeBtn);
-        
-        card.appendChild(title);
-        card.appendChild(author);
-        card.appendChild(pages);
-        card.appendChild(btnGrp);
-        
-        cardGroup.appendChild(card);
+console.log(myLibrary);
 
-        // Create event listeners
-        removeBtn.addEventListener('click', removeBookFromLibrary);
-        toggleBtn.addEventListener('click', toggleRead);
-    })
-}
+myLibrary.removeBook('The Living Mountain');
+console.log(myLibrary);
 
-displayLibrary();
 
-function addBookToLibrary(e) {
-    e.preventDefault();
-    
-    const bookTitle = title.value;
-    const bookAuthor = author.value;
-    const bookPages = Number(pages.value);
-    let bookIsRead = isRead.checked;
-    
-    const newBook = new Book(
-        bookTitle,
-        bookAuthor,
-        bookPages,
-        bookIsRead
-        )
+// displayLibrary() {
+//     // Add card for each book in library
+//     this.books.forEach(function(book, index) {
         
-        if (myLibrary.some(item => item.title === bookTitle && item.author === bookAuthor)) {
-            alert('Book already exists in your library') 
-        } else {
-            myLibrary.push(newBook);
-        }
+//         // Create card and child elements
+//         const card = document.createElement('div');
+//         const title = document.createElement('p');
+//         const author = document.createElement('p');
+//         const pages = document.createElement('p');
+//         const toggleBtn = document.createElement('button');
+//         const removeBtn = document.createElement('button');
+//         const btnGrp = document.createElement('div');
         
-        displayLibrary();
-        modal.close();
+//         // Set attributes on elements
+//         card.className = 'card';
+//         card.setAttribute('data-index', index);
+//         title.className = 'title';
+//         author.className = 'author';
+//         pages.className = 'pages';
+//         toggleBtn.className = 'toggle';
+//         removeBtn.className = 'remove';
+//         btnGrp.className = 'button-group';
         
-        // Reset form values
+//         if (book.isRead === true) {
+//             toggleBtn.setAttribute('aria-pressed', true);
+//         } else {
+//             toggleBtn.setAttribute('aria-pressed', false);
+//         }
         
-        title.value = '';
-        author.value = '';
-        pages.value = '';
-        isRead.checked = false;
-}
-    
-function removeBookFromLibrary(e) {
-    myLibrary.splice(e.target.parentElement.parentElement.getAttribute('data-index'), 1);
+//         // Add text 
+//         title.appendChild(document.createTextNode(item.title));
+//         author.appendChild(document.createTextNode(item.author));
+//         pages.appendChild(document.createTextNode(item.pages + ' pages'));
+//         removeBtn.appendChild(document.createTextNode('Remove'));
+        
+//         // Add elements to DOM    
+//         btnGrp.appendChild(toggleBtn);
+//         btnGrp.appendChild(removeBtn);
+        
+//         card.appendChild(title);
+//         card.appendChild(author);
+//         card.appendChild(pages);
+//         card.appendChild(btnGrp);
+        
+//         cardGroup.appendChild(card);
 
-    displayLibrary();
-}
+//         removeBtn.addEventListener('click', removeBookFromLibrary);
+//         toggleBtn.addEventListener('click', toggleRead);
+//     });
+// }
 
-function toggleRead(e) {
-    if (e.target.ariaPressed === 'true') {
-        e.target.ariaPressed = 'false';
-        myLibrary[e.target.parentElement.parentElement.getAttribute('data-index')].isRead = false;
-    } else if (e.target.ariaPressed === 'false') {
-        e.target.ariaPressed = 'true';
-        myLibrary[e.target.parentElement.parentElement.getAttribute('data-index')].isRead = true;
-    }
-}
+// function getBookInfo(e) {
+//     e.preventDefault();
     
-// Event listeners
-    
-openModal.addEventListener('click', () => {
-    modal.showModal();
-})
-    
-closeModal.addEventListener('click', () => {
-    modal.close();
-})
+//         const bookTitle = title.value;
+//         const bookAuthor = author.value;
+//         const bookPages = Number(pages.value);
+//         let bookIsRead = isRead.checked;
+        
+//         const newBook = new Book(
+//             bookTitle,
+//             bookAuthor,
+//             bookPages,
+//             bookIsRead
+//         )
+            
+//         if (myLibrary.some(item => item.title === bookTitle && item.author === bookAuthor)) {
+//             alert('Book already exists in your library') 
+//         } else {
+//             myLibrary.push(newBook);
+//         }
+            
+//         displayLibrary();
+//         modal.close();
+            
+//         // Reset form values
+        
+//         title.value = '';
+//         author.value = '';
+//         pages.value = '';
+//         isRead.checked = false;
+//         }
 
-form.addEventListener('submit', addBookToLibrary);
+//     removeBookFromLibrary(e) {
+//         myLibrary.splice(e.target.parentElement.parentElement.getAttribute('data-index'), 1);
+//         myLibrary.displayLibrary();
+//     }
+    
+// // Event listeners
+    
+// openModal.addEventListener('click', () => {
+//     modal.showModal();
+// })
+    
+// closeModal.addEventListener('click', () => {
+//     modal.close();
+// })
+
+// form.addEventListener('submit', () => {getBookInfo});
+
+
 
 
 
