50fb5ad59a4c6189e7d8e8b50dcd3601c2f36cd2
Lauren Champs <laurenkchamps@gmail.com>
2023-12-04 17:09:07 +1030

    Update local storage in _removeBook()

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const inputAuthor = document.getElementById('book-author');
 const inputPages = document.getElementById('book-pages');
 const inputRead = document.getElementById('read-checkbox');
 
-class App {
+class Library {
   #books = [];
 
   constructor() {
@@ -64,7 +64,7 @@ class App {
     .split(' ')
     .map((word) => word[0].toUpperCase() + word.slice(1))
     .join(' ');
-}
+  }
 
   _newBook(e) {
     e.preventDefault();
@@ -112,7 +112,6 @@ class App {
         </div>
       </div>
     `
-
     cardGroup.insertAdjacentHTML('afterbegin', html)
   }
 
@@ -125,6 +124,9 @@ class App {
 
     // Remove card from books array
     this.#books = this.#books.filter(book => book.id !== card.dataset.id);
+
+    // Update local storage
+    localStorage.setItem('books', JSON.stringify(this.#books));
 
     // Remove card from UI
     card.remove();
@@ -183,174 +185,7 @@ class App {
     this.#books.push(girl);
     this.#books.push(overstory);
   }
-
 }
 
-const app = new App();
-
-// class Library {
-//   constructor() {
-//     this.books = [];
-//   }
-
-//   _inLibrary(book) {
-//     return this.books.some(
-//       b => b.title === book.title && b.author === book.author
-//     );
-//   }
-
-//   addBook(book) {
-//     if (!this._inLibrary(book)) {
-//       this.books.push(book);
-//     } else {
-//       alert(`${book.title} is already in your library`);
-//       resetForm();
-//     }
-//     return this;
-//   }
-
-//   removeBook(title, author) {
-//     this.books = this.books.filter(
-//       b => b.title !== title && b.author !== author
-//     );
-//   }
-
-//   getBooks() {
-//     return library.books;
-//   }
-// }
-
-// const library = new Library();
-
-// // User Interface
-
-// const titleCaseify = function(str) {
-//   return str
-//     .toLowerCase()
-//     .split(' ')
-//     .map((word) => word[0].toUpperCase() + word.slice(1))
-//     .join(' ');
-// }
-
-// const createBook = function(e) {
-//   e.preventDefault();
-
-//   const newBook = new Book(titleCaseify(title.value), titleCaseify(author.value), Number(pages.value), isRead.checked);
-
-//   return newBook;
-// }
-
-// const setDefaultBooks = function() {
-//   const nineteen84 = new Book('Nineteen Eighty-Four', 'George Orwell', 356, true);
-//   nineteen84.id = '0000000001';
-//   const girl = new Book('Girl, Woman, Other', 'Bernadine Evaristo', 464, true);
-//   girl.id = '0000000002';
-//   const overstory = new Book('The Overstory', 'Richard Powers', 625, false);
-//   overstory.id = '0000000003';
-
-//   library.addBook(nineteen84).addBook(girl).addBook(overstory);
-// }
-
-// const clearBooks = function() {
-//   while (cardGroup.firstElementChild) {
-//     cardGroup.removeChild(cardGroup.firstElementChild);
-//   }
-// };
-
-// const toggleRead = function(e, book) {
-//   if (e.target.ariaPressed === 'true') {
-//     book.updateRead(false);
-//     e.target.ariaPressed = 'false';
-//   } else {
-//     book.updateRead(true);
-//     e.target.ariaPressed = 'true';
-//   }
-// };
-
-// const displayBooks = function(library) {
-//   library.books.forEach(book => {
-//     // Create card and child elements
-//     const card = document.createElement('div');
-//     const title = document.createElement('p');
-//     const author = document.createElement('p');
-//     const pages = document.createElement('p');
-//     const toggleBtn = document.createElement('button');
-//     const removeBtn = document.createElement('button');
-//     const btnGrp = document.createElement('div');
-//     // Set attributes on elements
-//     card.className = 'card';
-//     title.className = 'title';
-//     author.className = 'author';
-//     pages.className = 'pages';
-//     toggleBtn.className = 'toggle';
-//     removeBtn.className = 'remove';
-//     btnGrp.className = 'button-group';
-
-//     if (book.isRead === true) {
-//       toggleBtn.setAttribute('aria-pressed', true);
-//     } else {
-//       toggleBtn.setAttribute('aria-pressed', false);
-//     }
-
-//     // Add text
-//     title.appendChild(document.createTextNode(book.title));
-//     author.appendChild(document.createTextNode(book.author));
-//     pages.appendChild(document.createTextNode(book.pages + ' pages'));
-//     removeBtn.appendChild(document.createTextNode('Remove'));
-
-//     // Add elements to DOM
-//     btnGrp.appendChild(toggleBtn);
-//     btnGrp.appendChild(removeBtn);
-
-//     card.appendChild(title);
-//     card.appendChild(author);
-//     card.appendChild(pages);
-//     card.appendChild(btnGrp);
-
-//     cardGroup.appendChild(card);
-
-//     removeBtn.addEventListener('click', function() {
-//       library.removeBook(book.title, book.author);
-//       clearBooks();
-//       displayBooks(library);
-//     });
-
-//     toggleBtn.addEventListener('click', function(e) {
-//       toggleRead(e, book);
-//     });
-//     }
-//   )};
-
-// const resetForm = function() {
-//   title.value = '';
-//   author.value = '';
-//   pages.value = '';
-//   isRead.checked = false;
-// }
-
-// const init = function() {
-//   setDefaultBooks();
-//   displayBooks(library);
-//   console.log(library.books);
-// }
-
-// // Event listeners
-
-// form.addEventListener('click', () => {
-//   modal.showModal();
-// });
-// closeModal.addEventListener('click', () => {
-//   modal.close();
-// });
-
-// document.querySelector('.book-form').addEventListener('submit', function(e) {
-//   library.addBook(createBook(e));
-//   clearBooks();
-//   displayBooks(library);
-//   resetForm();
-//   modal.close();
-// });
-
-// document.addEventListener('DOMContentLoaded', init);
-
+const library = new Library();
 
