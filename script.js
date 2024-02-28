f97c45a56e8f935cb9cc02b9fa6a93eecfaccbe5
Lauren Champs <laurenkchamps@gmail.com>
2024-02-28 13:15:21 +1030

    Update default books

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 class Book {
-  
   constructor(
     id = Date.now().toString().slice(-10),
     title = 'Unknown',
@@ -54,16 +53,16 @@ class Library {
 
   _inLibrary(book) {
     return this.#books.some(
-      b => b.title === book.title && b.author === book.author
+      (b) => b.title === book.title && b.author === book.author
     );
   }
 
   _titleCaseify(str) {
-  return str
-    .toLowerCase()
-    .split(' ')
-    .map((word) => word[0].toUpperCase() + word.slice(1))
-    .join(' ');
+    return str
+      .toLowerCase()
+      .split(' ')
+      .map((word) => word[0].toUpperCase() + word.slice(1))
+      .join(' ');
   }
 
   _newBook(e) {
@@ -74,15 +73,22 @@ class Library {
     const author = inputAuthor.value;
     const pages = +inputPages.value;
     const isRead = inputRead.checked;
-    
-    if(!title || !author) return alert('Please enter title and author');
+
+    if (!title || !author) return alert('Please enter title and author');
 
     // Create new book
-    const book = new Book(undefined, this._titleCaseify(title), this._titleCaseify(author), pages, isRead);
-    
+    const book = new Book(
+      undefined,
+      this._titleCaseify(title),
+      this._titleCaseify(author),
+      pages,
+      isRead
+    );
+
     // Check for existing book of same name and author
-    if(this._inLibrary(book)) return alert(`${book.title} is already in your library`);
-    
+    if (this._inLibrary(book))
+      return alert(`${book.title} is already in your library`);
+
     // Add book to books array
     this.#books.push(book);
 
@@ -97,7 +103,7 @@ class Library {
 
     // Clear form fields
     inputTitle.value = inputAuthor.value = inputPages.value = '';
-    inputRead.checked = false;    
+    inputRead.checked = false;
   }
 
   _renderBook(book) {
@@ -107,23 +113,25 @@ class Library {
         <p class="author">${book.author}</p>
         <p class="pages">${book.pages} pages</p>
         <div class="button-group">
-          <button class="toggle" aria-pressed="${book.isRead === true ? true : false}"></button>
+          <button class="toggle" aria-pressed="${
+            book.isRead === true ? true : false
+          }"></button>
           <button class="remove">Remove</button>
         </div>
       </div>
-    `
-    cardGroup.insertAdjacentHTML('afterbegin', html)
+    `;
+    cardGroup.insertAdjacentHTML('afterbegin', html);
   }
 
   _removeBook(e) {
-    const removeBtn = e.target.closest('.remove')
-    
-    if(!removeBtn) return;
+    const removeBtn = e.target.closest('.remove');
+
+    if (!removeBtn) return;
 
     const card = removeBtn.closest('.card');
 
     // Remove card from books array
-    this.#books = this.#books.filter(book => book.id !== card.dataset.id);
+    this.#books = this.#books.filter((book) => book.id !== card.dataset.id);
 
     // Update local storage
     localStorage.setItem('books', JSON.stringify(this.#books));
@@ -135,23 +143,23 @@ class Library {
   _toggleReadField(e) {
     const toggleBtn = e.target.closest('.toggle');
 
-    if(!toggleBtn) return;
+    if (!toggleBtn) return;
 
     const cardId = toggleBtn.closest('.card').dataset.id;
 
-    if(toggleBtn.ariaPressed === 'true') {
+    if (toggleBtn.ariaPressed === 'true') {
       // Toggle aria-pressed value
       toggleBtn.ariaPressed = 'false';
       // Update isRead field in books array
-      this.#books.forEach(book => {
-        if(book.id === cardId) book.setRead(false)
+      this.#books.forEach((book) => {
+        if (book.id === cardId) book.setRead(false);
       });
     } else if (toggleBtn.ariaPressed === 'false') {
       // Toggle aria-pressed value
       toggleBtn.ariaPressed = 'true';
       // Update isRead field in books array
-      this.#books.forEach(book => {
-        if(book.id === cardId) book.setRead(true)
+      this.#books.forEach((book) => {
+        if (book.id === cardId) book.setRead(true);
       });
     }
 
@@ -165,29 +173,52 @@ class Library {
   _getLocalStorage() {
     const data = JSON.parse(localStorage.getItem('books'));
 
-    if(!data) {
+    if (!data) {
       this._setDefaultBooks();
     } else {
-        // Convert book objects back into book class
-    data.forEach((book) => {
-      const newBook = new Book(book.id, book.title, book.author, book.pages, book.isRead);
-      this.#books.push(newBook);
-    })
+      // Convert book objects back into book class
+      data.forEach((book) => {
+        const newBook = new Book(
+          book.id,
+          book.title,
+          book.author,
+          book.pages,
+          book.isRead
+        );
+        this.#books.push(newBook);
+      });
     }
 
-    this.#books.forEach(book => this._renderBook(book));    
+    this.#books.forEach((book) => this._renderBook(book));
   }
 
   _setDefaultBooks() {
-    const nineteen84 = new Book('0000000001', 'Nineteen Eighty-Four', 'George Orwell', 356, true);
-    const girl = new Book('0000000002', 'Girl, Woman, Other', 'Bernadine Evaristo', 464, true);
-    const overstory = new Book('0000000003', 'The Overstory', 'Richard Powers', 625, false);
-    
+    const nineteen84 = new Book(
+      '0000000001',
+      'Nineteen Eighty-Four',
+      'George Orwell',
+      356,
+      true
+    );
+    const girl = new Book(
+      '0000000002',
+      'Girl, Woman, Other',
+      'Bernadine Evaristo',
+      464,
+      true
+    );
+    const rifqa = new Book(
+      '0000000003',
+      'Rifqa',
+      'Mohammed El-Kurd',
+      97,
+      false
+    );
+
     this.#books.push(nineteen84);
     this.#books.push(girl);
-    this.#books.push(overstory);
+    this.#books.push(rifqa);
   }
 }
 
 const library = new Library();
-
